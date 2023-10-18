9448537e4a67860e8fa960d37a91366d50f214cb
YuMZyX <joni1koskinen@gmail.com>
2023-10-18 21:45:14 +0300

    11.9 update

--- full-stack-open-pokedex/cypress/e2e/pokedex.cy.js
+++ full-stack-open-pokedex/cypress/e2e/pokedex.cy.js
@@ -4,4 +4,14 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it ('navigating to the page of a particular Pokemon works', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('Previous')
+    cy.contains('Next')
+    cy.contains('Home')
+    cy.contains('overgrow')
+    cy.contains('chlorophyll')
+  })
 })
