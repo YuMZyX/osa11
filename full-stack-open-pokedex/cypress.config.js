6499907fdb3ac662b1b361960c7744f7122984ff
YuMZyX <joni1koskinen@gmail.com>
2023-10-18 21:34:23 +0300

    11.9 update

--- full-stack-open-pokedex/cypress.config.js
+++ full-stack-open-pokedex/cypress.config.js
@@ -2,4 +2,9 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   video: false,
+  e2e: {
+    setupNodeEvents(_on, _config) {
+      // implement node event listeners here
+    },
+  },
 })
