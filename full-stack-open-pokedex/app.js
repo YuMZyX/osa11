18c2f9403a0eac59e98de2fe5baeef8f5fac5d2b
YuMZyX <joni1koskinen@gmail.com>
2023-10-19 15:30:50 +0300

    11.12 simulating broken deployment

--- full-stack-open-pokedex/app.js
+++ full-stack-open-pokedex/app.js
@@ -7,13 +7,13 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('3') // change this string to ensure a new version deployed
+  res.send('4') // change this string to ensure a new version deployed
 })
 
 app.get('/health', (req, res) => {
-  //res.status(404).json({ error: 'error' })
+  res.status(404).json({ error: 'error' })
   // eslint-disable-next-line no-unreachable
-  res.send('ok')
+  //res.send('ok')
 })
 
 app.listen(PORT, () => {
